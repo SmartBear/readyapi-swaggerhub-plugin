6ad60cd6d470bd3513795624bdcb752b0db0e922
Joel Jonsson <joel.jonsson@smartbear.com>
2015-09-30 08:05:11 +0200

    Depend on the Swagger Plugin rather than including it.

--- src/main/java/com/smartbear/plugins/swaggerhub/PluginConfig.java
+++ src/main/java/com/smartbear/plugins/swaggerhub/PluginConfig.java
@@ -2,10 +2,15 @@ package com.smartbear.plugins.swaggerhub;
 
 import com.eviware.soapui.plugins.PluginAdapter;
 import com.eviware.soapui.plugins.PluginConfiguration;
+import com.eviware.soapui.plugins.PluginDependencies;
+import com.eviware.soapui.plugins.PluginDependency;
 
 @PluginConfiguration(groupId = "com.smartbear.plugins", name = "SwaggerHub ReadyAPI Plugin", version = "1.0.0",
         autoDetect = true, description = "Integrates Ready API with SwaggerHub",
         infoUrl = "")
+@PluginDependencies({
+        @PluginDependency(groupId = "com.smartbear.soapui.plugins", name = "Swagger Plugin", minimumVersion = "2.1.3"),
+})
 public class PluginConfig extends PluginAdapter {
 
     public final static String SWAGGERHUB_URL = "https://swaggerhub.com";
